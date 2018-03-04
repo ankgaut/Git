745ed669014329f1b4b257fa72dae141873f7f11
Ankur Gautam <ankurgautam379@gmail.com>
2018-03-04 22:54:22 +0530

    user_2: step 13

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+void microkernel_sendmsg(char *);
+
 void main(){
     printf("HelloWorld!\n");
+    microkernel_sendmsg("is more protable");
+}
+
+void microkernel_sendmsg(char *a){
+    printf("microkernel:%s\n",a);
 }
