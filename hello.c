c0c6aa1c0e6c5c93469271451e9effbe51cbf262
Ankur Gautam <ankurgautam379@gmail.com>
2018-03-04 23:45:31 +0530

    user_1: step 21

--- hello.c
+++ hello.c
@@ -1,13 +1,7 @@
 #include<stdio.h>
 
-void microkernel_sendmsg(char *);
-
 void main(){
     printf("HelloWorld!\n");
     printf("This must be a monolithic design\n");
-    microkernel_sendmsg("is more protable");
 }
 
-void microkernel_sendmsg(char *a){
-    printf("microkernel:%s\n",a);
-}
